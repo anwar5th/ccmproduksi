5999ac66449d475177504992bf14371009aeda3f
anwar5th <anwar5th@gmail.com>
2023-07-31 17:33:56 +0700

    memperbaiki search

--- app/Http/Controllers/ListSpkController.php
+++ app/Http/Controllers/ListSpkController.php
@@ -26,11 +26,13 @@ class ListSpkController extends Controller
      *
      * @return View
      */
-    public function index(): View
+    public function index(Request $request): View
     {
 
+        //get search
+        $keyword = $request->keyword;
         //get posts
-        $listspk = Antrianmesin::latest()->paginate(5);
+        $listspk = Antrianmesin::where('nospk', 'LIKE', '%'.$keyword.'%')->latest()->paginate(5);
 
         //render view with posts
         return view('listspk.index', compact('listspk')); //text warna oranye "antrianmesin"(folder) didapat dari folder /resources/views/antrianmesin dan text index didapat dari file "index" berada di /resources/views/antrianmesin/index.blade.php
--- resources/views/proyekorders/index.blade.php
+++ resources/views/proyekorders/index.blade.php
@@ -24,7 +24,7 @@
                                         <div class="col-12 col-sm-8 col-md-5">
                                         <form action="" method="GET">
                                             <div class="input-group mb-3">
-                                              <input name="keyword" class="form-control" placeholder="Pencarian Berdasarkan Nama PO atau Kode PO, disini" aria-label="Pencarian Berdasarkan Nama PO atau Kode PO, disini" aria-describedby="button-addon2" value="">
+                                              <input name="keyword" class="form-control" placeholder="Pencarian Berdasarkan Nama PO atau Tanggal PO, disini" aria-label="Pencarian Berdasarkan Nama PO atau Tanggal PO, disini" aria-describedby="button-addon2" value="">
                                             </div>
                                         </form>
                                         </div>
