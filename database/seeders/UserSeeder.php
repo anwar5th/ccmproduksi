7c0b02d64a204e5026e8ce4fffc7dc0683036e7e
arulabdulaziz <rulz.matrixs@gmail.com>
2026-05-16 07:32:44 +0700

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => '$2y$10$YaHPyXEi6yt1IfNEJ/OqoOG2mkYWrys6WdI4lz61QZW8UNOTDStPS',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@user.com',
+                'password' => '$2y$10$HVUecFZp5nW14SqgP0H1UORLkHmusVibHzxoIbxwr3eolXffcFiq.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
